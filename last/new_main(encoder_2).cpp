cb6493be418ce37fe71bba79311ce749fe947ea7
kihaken <153414005+kihaken@users.noreply.github.com>
2024-02-15 09:13:51 +0900

    new_main(encoder_2).cpp を更新

--- last/new_main(encoder_2).cpp
+++ last/new_main(encoder_2).cpp
@@ -1,4 +1,4 @@
-// ここではkeyのリミットスイッチは基準となる場所にのみつける予定 (エンコーダーを3個使用)
+// ここではkeyのリミットスイッチは基準点のみつける予定 (エンコーダーを3個使用)
 // これはトルク制御 (たぶん速度制御できる余裕ない)
 
 #include "mbed.h"
@@ -21,8 +21,11 @@ rbms orb(can, 0, 1);
 rbms key(can, 0, 2);
 InterruptIn a(p27); // エンコーダー用のピン宣言
 InterruptIn b(p6);
-DigitalIn limit_x(p15), limit_y(p16);
-
+// 基準点, 目標点
+DigitalIn limit_x[2] = {DigitalIn(p15), 
+						DigitalIn(p16)};
+DigitalIn limit_y[2] = {DigitalIn(p17),
+						DigitalIn(p18)};
 void reference_pc();
 void reference_ps3();
 void front(); // 足回り
@@ -41,6 +44,7 @@ void encoder(float *angle); // エンコーダー
 void angle_reset();
 void a_slit();
 void b_slit();
+void limit_update(DigitalIn limit, Limit_Type type);
 
 int i = 0;
 int _orb[1] = {0}; 
@@ -50,31 +54,37 @@ int data[8] = {0}; // PS3
 float angle = 0; 
 int passed_slit = 0; // エンコーダー
 enum Encoder_Type{ // どの機構に関するエンコーダーか
+	None,
     orb_rote,
     key_x_rote,
     key_y_rote
 } encoder_type;
+encoder_type = None;
+enum Limit_Type{
+	none,
+	x_zero,
+	x_goal,
+	y_zero,
+	y_goal
+}limit_type;
+limit_type = none;
 
 int main(){
     angle_reset();
     while(1){
         // リミットスイッチの状態をチェック
-        if (limit_x){ // 各リミットスイッチがそれぞれhighの時
-            _key[0] = 0;
-            angle_reset();
-        }
-        if (limit_y){
-            _key[1] = 0;
-            angle_reset();       
-        }
+        for(i = 0;i < 2;i++){
+			limit_update(limit_x[i],limit_type);
+			limit_update(limit_y[i],limit_type);
+		}
         encoder(&angle); // 角度取得後、angleに格納
         reference_pc();
         reference_ps3();
         // 指定した角度分回っていたら止める(brakeで上書き)
         switch (encoder_type){
-            case orb_rote : if (angle == ORB_ROTE) _orb[0] = 0; break;
-            case key_x_rote : if (angle == KEY_X_ROTE) _key[0] = 0; break;
-            case key_y_rote : if (angle == KEY_Y_ROTE)  _key[1] = 0; break;
+            case orb_rote : if (angle == ORB_ROTE) {_orb[0] = 0; break;}
+            case key_x_rote : if (angle == KEY_X_ROTE) {_key[0] = 0; break;}
+            case key_y_rote : if (angle == KEY_Y_ROTE)  {_key[1] = 0; break;}
         }
         crawler.rbms_send(_crawler); // 制御信号の送信
         orb.rbms_send(_orb);
@@ -140,21 +150,25 @@ void orb_drop(){
 void key_catch(){
     _key[0] = KEY_X_ROTE;
     encoder_type = key_x_rote;
+    limit_type = x_goal;
 }
 
 void key_release(){
     _key[0] = -KEY_X_ROTE;
     encoder_type = key_x_rote;
+    limit_type = x_zero;
 }
 
 void key_up(){
     _key[1] = KEY_Y_ROTE;
     encoder_type = key_y_rote;
+    limit_type = y_goal;
 }
 
 void key_down(){
     _key[1] = -KEY_Y_ROTE;
     encoder_type = key_y_rote;
+    limit_type = y_zero;
 }
 
 void encoder(float *angle){
@@ -189,6 +203,17 @@ void b_slit(){
 
 void angle_reset(){
     angle = 0;
+}
+
+void limit_update(DigitalIn limit, Limit_Type type)
+{
+	if (type == none) return;
+	switch (type){
+		case x_zero : 
+		case x_goal : if (limit) {_key[0] = 0; angle_reset(); break;}
+		case y_zero : 
+		case y_goal : if (limit) {_key[1] = 0; angle_reset(); break;}
+	}
 }
 
 void reference_pc(){
