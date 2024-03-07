8bd5c5976d080ecfab9f32803e9258e984779adc
kihaken <153414005+kihaken@users.noreply.github.com>
2024-03-07 09:19:11 +0900

    get_rotation.cpp を更新

--- ロボマス 動作確認/get_rotation.cpp
+++ ロボマス 動作確認/get_rotation.cpp
@@ -21,6 +21,7 @@ rote_robo_ms rote_robo_ms_motor1(can, test, 0, 16);
 rote_robo_ms rote_robo_ms_motor2(can, test, 1, 16);
 Thread thread1, thread2, thread_can;
 
+void set_all_static_reset();
 void motor1_func();
 void motor2_func();
 void can_receive();
@@ -33,23 +34,25 @@ long motor2_rotation = 0;
 
 int main(){
     motor[0] = 1000;
-    motor[1] = 1500;
+    motor[1] = 2000;
     thread1.start(motor1_func);
     thread2.start(motor2_func);
     thread_can.start(can_receive);
-
-    rote_robo_ms_motor1.set_static_reset(0);
-    rote_robo_ms_motor2.set_static_reset(0);
-
+	set_all_static_reset();
     while(true){
         ThisThread::sleep_for(10ms);
     }
 }
 
+void set_all_static_reset(){
+	rote_robo_ms_motor1.set_static_reset(0);
+	rote_robo_ms_motor2.set_static_reset(0);
+}
+
 void motor1_func(){
     while (true){
-        rote_robo_ms_motor1.rote_robo_ms_update(buffer_motor1, BUFFER_MAX);
-        test.rbms_send(motor);
+        rote_robo_ms_motor1.rote_robo_ms_update(buffer_motor1, BUFFER_MAX); // motor1の角度の更新
+        test.rbms_send(&motor[0]); // 制御信号の送信
 
         motor1_angle = rote_robo_ms_motor1.get_rote_sum();
         motor1_rotation = rote_robo_ms_motor1.get_rote_sum() / GIAR_RATIO; 
@@ -64,8 +67,8 @@ void motor1_func(){
 
 void motor2_func(){
     while (true){
-        rote_robo_ms_motor2.rote_robo_ms_update(buffer_motor2, BUFFER_MAX);
-        test.rbms_send(motor);
+        rote_robo_ms_motor2.rote_robo_ms_update(buffer_motor2, BUFFER_MAX); // motor2の角度の更新
+        test.rbms_send(&motor[1]); // 制御信号の送信
 
         motor2_angle = rote_robo_ms_motor2.get_rote_sum();
         motor2_rotation = rote_robo_ms_motor2.get_rote_sum() / GIAR_RATIO; 
